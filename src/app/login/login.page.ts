82c0b84f7304f6c34fdda4d3d07b80466590f317
vini121423 <vinicius.santos838@etec.sp.gov.br>
2020-08-19 19:36:53 -0300

    Criação do método de login com validação

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+   
+  // Armazena informações do formulário
+  public formLogin: FormGroup;
+  
+  public mensagens_validacao = {
+	email:[
+    {tipo:'required', mensagem:'O campo e-mail é obrigatório!'},
+	{tipo:'email',mensagem:'E-mail inválido!'}
+    ],
+	senha:[
+	{tipo:'required',mensagem:'O campo senha é obrigatório!'},
+	{tipo:'minLength',mensagem:'A senha deve possuir pelo menos 6 caracteres !'}
+	]
+  }
 
-  constructor() { }
+  constructor(
+    private formBuilder:FormBuilder,
+	private router:Router
+	) { 
+	 this.formLogin = formBuilder.group({
+		email:['',Validators.compose([Validators.required, Validators.email])],
+        senha:['',Validators.compose([Validators.required, Validators.minLength(6)])], 		
+	 })
+	
+	}
 
   ngOnInit() {
+  }
+  
+  public login(){
+	if(this.formLogin.valid){
+      console.log('Formulário válido');
+	  this.router.navigateByUrl('/home');
+	} else{
+      console.log('Formulário inválido');
+	}		
   }
 
 }
