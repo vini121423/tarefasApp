28a8b281a04bb8a70f46630b15e521524fa29785
vini121423 <vinicius.santos838@etec.sp.gov.br>
2020-08-22 13:07:03 -0300

    Validação de campos

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -8,42 +8,42 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-   
+
   // Armazena informações do formulário
   public formLogin: FormGroup;
-  
+
   public mensagens_validacao = {
-	email:[
-    {tipo:'required', mensagem:'O campo e-mail é obrigatório!'},
-	{tipo:'email',mensagem:'E-mail inválido!'}
+    email: [
+      { tipo: 'required', mensagem: 'O campo e-mail é obrigatório!' },
+      { tipo: 'email', mensagem: 'E-mail inválido!' }
     ],
-	senha:[
-	{tipo:'required',mensagem:'O campo senha é obrigatório!'},
-	{tipo:'minLength',mensagem:'A senha deve possuir pelo menos 6 caracteres !'}
-	]
-  }
+    senha: [
+      { tipo: 'required', mensagem: 'O campo senha é obrigatório!' },
+      { tipo: 'minlength', mensagem: 'A senha deve possuir pelo menos 6 caracteres !' }
+    ]
+  };
 
   constructor(
-    private formBuilder:FormBuilder,
-	private router:Router
-	) { 
-	 this.formLogin = formBuilder.group({
-		email:['',Validators.compose([Validators.required, Validators.email])],
-        senha:['',Validators.compose([Validators.required, Validators.minLength(6)])], 		
-	 })
-	
-	}
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.formLogin = formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      senha: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+    });
+
+  }
 
   ngOnInit() {
   }
-  
-  public login(){
-	if(this.formLogin.valid){
+
+  public login() {
+    if (this.formLogin.valid) {
       console.log('Formulário válido');
-	  this.router.navigateByUrl('/home');
-	} else{
+      this.router.navigateByUrl('/home');
+    } else {
       console.log('Formulário inválido');
-	}		
+    }
   }
 
 }
--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,15 +1,75 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
-  selector: 'app-registro',
-  templateUrl: './registro.page.html',
-  styleUrls: ['./registro.page.scss'],
+	selector: 'app-registro',
+	templateUrl: './registro.page.html',
+	styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+	public formRegister: FormGroup;
 
-  ngOnInit() {
-  }
+	public validacao = {
+		nome: [
+			{ tipo: 'required', msg: 'Campo obrigatório!' },
+			{ tipo: 'minlength', msg: 'Insira no mínimo 3 caracteres!' }
+		],
+		cpf: [
+			{ tipo: 'required', msg: 'Campo obrigatório!' },
+			{ tipo: 'minlength', msg: 'Insira no mínimo 11 caracteres ou no máximo 14!' },
+			{ tipo: 'maxlength', msg: 'Insira no mínimo 11 caracteres ou no máximo 14!' }
+		],
+		data_nascimento: [
+			{ tipo: 'required', msg: 'Campo obrigatório!' }
+		],
+		genero: [
+			{ tipo: 'required', msg: 'Campo obrigatório!' }
+		],
+		celular: [
+			{ tipo: 'maxlength', msg: 'Insira no máximo 16 caracteres!' }
+		],
+		email: [
+			{ tipo: 'required', msg: 'Campo obrigatório!' },
+			{ tipo: 'email', msg: 'E-mail inválido!' }
+		],
+		senha: [
+			{ tipo: 'required', msg: 'Campo obrigatório!' },
+			{ tipo: 'minlength', msg: 'Insira no mínimo 6 caracteres!' },
+		],
+		senha_confirm: [
+			{ tipo: 'required', msg: 'Campo obrigatório!' },
+			{ tipo: 'minlength', msg: 'Insira no mínimo 6 caracteres!' },
+		]
+	};
+
+	constructor(private formBuilder: FormBuilder, private route: Router) {
+
+		this.formRegister = formBuilder.group({
+			nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+			cpf: ['', Validators.compose([Validators.required, Validators.minLength(11), Validators.maxLength(14)])],
+			data_nascimento: ['', Validators.compose([Validators.required])],
+			genero: ['', Validators.compose([Validators.required])],
+			celular: ['', Validators.compose([Validators.maxLength(16)])],
+			email: ['', Validators.compose([Validators.required, Validators.email])],
+			senha: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+			senha_confirm: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+		});
+
+	}
+
+	ngOnInit() {
+	}
+
+	public register() {
+		if (this.formRegister.valid) {
+			console.log('Formulário válido');
+			this.route.navigateByUrl('/home');
+		} else {
+			console.log('Formulário inválido');
+		}
+	}
 
 }
